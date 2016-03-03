9c9ccecf15614933dff3cf2ee29f778468385739
Ricardo Voigt <voigtt@gmail.com>
2016-03-02 21:35:33 -0300

    Add TC011

--- seguradora/susegbackend/src/com/br/TestesIntegracao/CotacaoTest.java
+++ seguradora/susegbackend/src/com/br/TestesIntegracao/CotacaoTest.java
@@ -3,16 +3,22 @@ package com.br.TestesIntegracao;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.br.DAO.CotacaoDAO;
+import com.br.Model.Condutor;
 import com.br.Model.Cotacao;
 import com.br.Model.Localizacao;
 import com.br.Model.Segurado;
 import com.br.Model.Veiculo;
+
 
 public class CotacaoTest {
 	private int codigoCotacao;
@@ -20,6 +26,7 @@ public class CotacaoTest {
 	private Localizacao localizacao;
 	private Segurado segurado;
 	private Veiculo veiculo;
+	private Condutor condutor;
 	@Before
 	public void setUp() throws Exception {
 		cotacao = new Cotacao();
@@ -27,17 +34,6 @@ public class CotacaoTest {
 		localizacao = new Localizacao();
 		localizacao.setEndereco("Rua Helma Buettner", 69, "89041-010");
 		localizacao.setLocal("Blumenau", "SC", "Brasil");
-		segurado = new Segurado();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		 
-		segurado.setDados("Ricardo Voigt", 
-							"046.319.119-64", 
-							"M", 
-							new Date(sdf.parse("14/02/1988").getTime()), 
-							5, 
-							"", 
-							"");
-		
 	}
 	
 	@Test
@@ -74,7 +70,21 @@ public class CotacaoTest {
 		
 		assertTrue("Cotacao deve possuir 2 veiculos", cotacao.getVeiculos().size() ==2);
 	}
-
+	
+	@Test
+	public void testInserirCondutorNaCotacao() throws ParseException{
+		condutor = new Condutor();
+		condutor.setDados("Ricardo Voigt", "046.319.119-64", "M", 28);
+		condutor.setSituacao("S", "N");
+		condutor.setCotacao(codigoCotacao);
+		condutor.save();
+		List<Condutor> condutores = new ArrayList<Condutor>();
+		condutores.add(condutor);
+		cotacao.setCondutores(condutores);
+		
+		assertTrue("Salvar a cotacao sem erro", cotacao.getCondutores().size() == 1);
+	}
+	
 	@After
 	public void tearDown()  {
 				
